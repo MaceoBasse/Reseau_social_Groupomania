ce0ab41f99f3eb1ceed2c59030e54fe898ced197
MaceoBasse <basse.maceo@gmail.com>
2021-11-23 12:16:46 +0100

    Update

--- vue-auth/src/views/Setting.vue
+++ vue-auth/src/views/Setting.vue
@@ -7,7 +7,7 @@
       </div>
 
       <div class="max-w-2xl bg-white py-10 px-5 m-auto w-full mt-10">
-        <div class="text-3xl mb-6 text-center">Description</div>
+        <div class="text-xl mb-6 text-center">Modifier la description</div>
 
         <div class="grid grid-cols-2 gap-4 max-w-xl m-auto">
           <div class="col-span-2">
@@ -19,6 +19,7 @@
                 p-3
                 md:text-xl
                 w-full
+                h-20
               "
               placeholder="Message"
               v-model="outline"
@@ -35,99 +36,88 @@
           </div>
         </div>
       </div>
-      <div class="h-screen px-2">
-        <div
-          class="
-            max-w-md
-            mx-auto
-            bg-white
-            rounded-lg
-            overflow-hidden
-            md:max-w-lg
-          "
-        >
-          <div class="md:flex">
-            <div class="w-full">
-              <div class="p-4 border-b-2">
-                <span class="text-lg font-bold text-gray-600"
-                  >Créer un Post</span
-                >
-              </div>
-              <div class="p-3">
-                <div class="mb-2">
-                  <span>Image</span>
-                  <div
-                    class="
-                      relative
-                      h-40
-                      rounded-lg
-                      border-dashed border-2 border-gray-200
-                      bg-white
-                      flex
-                      justify-center
-                      items-center
-                      hover:cursor-pointer
-                    "
-                  >
-                    <div class="absolute">
-                      <div
-                        class="flex flex-col items-center"
-                        v-if="image == ''"
-                      >
-                        <i
-                          class="fas fa-cloud-upload-alt fa-3x text-gray-200"
-                        ></i>
-                        <span class="block text-gray-400 font-normal"
-                          >Joindre vos fichier ici</span
-                        >
-                        <span class="block text-gray-400 font-normal">ou</span>
-                        <span class="block text-blue-400 font-normal"
-                          >Parcourir les fichiers</span
-                        >
-                      </div>
 
-                      <div class="flex flex-col items-center" v-else>
-                        <i
-                          class="fas fa-cloud-upload-alt fa-3x text-gray-200"
-                        ></i>
-                        <span class="block text-gray-400 font-normal">
-                          {{ image.name }}
-                        </span>
-                      </div>
+      <div
+        class="max-w-md mx-auto bg-white rounded-lg overflow-hidden md:max-w-lg mt-5 mb-5"
+      >
+        <div class="md:flex">
+          <div class="w-full">
+            <div class="p-4 border-b-2">
+              <span class="text-lg font-bold text-gray-600"
+                >Modifier la photo de profil</span
+              >
+            </div>
+            <div class="p-3">
+              <div class="mb-2">
+                <span>Image</span>
+                <div
+                  class="
+                    relative
+                    h-40
+                    rounded-lg
+                    border-dashed border-2 border-gray-200
+                    bg-white
+                    flex
+                    justify-center
+                    items-center
+                    hover:cursor-pointer
+                  "
+                >
+                  <div class="absolute">
+                    <div class="flex flex-col items-center" v-if="image == ''">
+                      <i
+                        class="fas fa-cloud-upload-alt fa-3x text-gray-200"
+                      ></i>
+                      <span class="block text-gray-400 font-normal"
+                        >Déposer vos fichier ici</span
+                      >
+                      <span class="block text-gray-400 font-normal">ou</span>
+                      <span class="block text-blue-400 font-normal"
+                        >Parcourir les fichiers</span
+                      >
                     </div>
-                    <input
-                      type="file"
-                      class="h-full w-full opacity-0"
-                      name=""
-                      @change="onChangeFile"
-                    />
+
+                    <div class="flex flex-col items-center" v-else>
+                      <i
+                        class="fas fa-cloud-upload-alt fa-3x text-gray-200"
+                      ></i>
+                      <span class="block text-gray-400 font-normal">
+                        {{ image.name }}
+                      </span>
+                    </div>
                   </div>
-                  <div class="flex justify-between items-center text-gray-400">
-                    <span
-                      >Type de fichier accepté : .png .jpg .gif uniquement</span
-                    >
-                    <span class="flex items-center"
-                      ><i class="fa fa-lock mr-1"></i> sécurise</span
-                    >
-                  </div>
+                  <input
+                    type="file"
+                    class="h-full w-full opacity-0"
+                    name=""
+                    @change="onChangeFile"
+                  />
                 </div>
-                <div class="mt-3 text-center pb-3">
-                  <button
-                    @click="changePicture(user.userId)"
-                    class="
-                      w-full
-                      h-12
-                      text-lg
-                      w-32
-                      bg-blue-600
-                      rounded
-                      text-white
-                      hover:bg-blue-700
-                    "
+                <div class="flex justify-between items-center text-gray-400">
+                  <span
+                    >Type de fichier accepté : .png .jpg .gif uniquement</span
                   >
-                    Créer
-                  </button>
+                  <span class="flex items-center"
+                    ><i class="fa fa-lock mr-1"></i> sécurise</span
+                  >
                 </div>
+              </div>
+              <div class="mt-3 text-center pb-3">
+                <button
+                  @click="changePicture(user.userId)"
+                  class="
+                    w-full
+                    h-12
+                    text-lg
+                    w-32
+                    bg-blue-600
+                    rounded
+                    text-white
+                    hover:bg-blue-700
+                  "
+                >
+                  Créer
+                </button>
               </div>
             </div>
           </div>
